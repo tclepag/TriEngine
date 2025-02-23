8c0ad010614b3a84c4d58f3e3add705e724d7e93
Tyler LePage <lepagetyler@hotmail.com>
2025-02-22 19:42:37 -0600

    Rewriting ALUaEnvironment

--- TriEngine/engine/lua/ALuaEnvironment.h
+++ TriEngine/engine/lua/ALuaEnvironment.h
@@ -2,73 +2,38 @@
 // Created by lepag on 2/21/25.
 //
 
-#ifndef ALUAENVIORNMENT_H
-#define ALUAENVIORNMENT_H
+#ifndef ALUAENVIRONMENT_H
+#define ALUAENVIRONMENT_H
 
 #include <functional>
-#include <vector>
-
-#include <lua.hpp>
+#include <lua.h>
 #include <lualib.h>
 #include <lauxlib.h>
-#include <memory>
-#include <string>
-#include <stdexcept>
-#include <iostream>
-#include "ALuaScript.h"
 
 namespace tri::lua {
 
 class ALuaEnvironment {
 public:
-    using ALuaPtr = std::unique_ptr<ALuaScript>;
     using ALuaState = lua_State*;
-    using ALuaScripts = std::vector<ALuaPtr>;
-    struct LuaContext {
-        ALuaPtr* script;
-    };
-
     ALuaEnvironment();
     ~ALuaEnvironment();
 
-    ALuaPtr* loadLua(const std::string& lua_path);
-    ALuaScripts* getLuaScripts();
-    ALuaState& getLuaState();
+    ALuaState* getLuaState();
 
-    // Exposes a C++ integer to lua
-    void luaSet(const int *integer) const;
-    // Exposes a C++ boolean to lua
-    void luaSet(bool boolean) const;
-    // Exposes a C++ string to lua
-    void luaSet(const char *string) const;
-    // Exposes a C++ float to lua
-    void luaSet(const float *floating) const;
-    template<class Result, typename... Args>
-    // Exposes a C++ Function to lua with arguments
-    void luaSet(std::function<Result(Args...)> cFunction);
-    template<class CClass>
-    // Calls the static C++ luaInit function to construct
-    // a lua version of the C++ class provided
-    void luaConstructClass(CClass cClass);
+    void pushValue(int* integer);
+    void pushValue(float* number);
+    void pushValue(double* number);
+    void pushValue(bool* boolean);
+    void pushValue(const char* string);
+    template<typename Return, typename... Args>
+    void pushValue(std::function<Return*(Args...)> cFunction);
 
-    template<typename Result, typename... Args>
-    std::function<Result*(Args...)> wrapCFunction(Result (cFunction)(Args...));
-
-    void newLuaClass(const char* className) const;
-    void setLuaTable(int index) const;
-    void setLuaField(int index, const char* fieldName) const;
-    void setLuaGlobal(const char* globalName) const;
-    template<class CClass>
-    CClass* createUserDataFromClass(CClass* cClass);
-    void releaseLua(const ALuaScript* lua);
-    void free() const;
-
-protected:
-    ALuaState p_state;
-    ALuaScripts scripts;
+    void registerLuaClass(const char* className);
+private:
+    ALuaState* m_luaState;
 };
 
 } // lua
 // tri
 
-#endif //ALUAENVIORNMENT_H
+#endif //ALUAENVIRONMENT_H
